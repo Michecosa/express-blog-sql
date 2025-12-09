b6329c76aeb476439a06681f06cea1f1900d4016
Michela <micheladellagatta1@gmail.com>
2025-12-09 16:06:52 +0100

    Hide all connection information

--- data/db.js
+++ data/db.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2");
 const password_segretissima =
   process.env.PASSWORD_CHE_AVREI_DOVUTO_NASCONDERE_20_COMMIT_FA;
+const user_segretissimo = process.env.USER;
+const host_segretissimo = process.env.HOST;
+const database_segretissimo = process.env.DATABASE;
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: host_segretissimo,
+  user: user_segretissimo,
   password: password_segretissima,
-  database: "149_blog_db",
+  database: database_segretissimo,
 });
 
 connection.connect((err) => {
